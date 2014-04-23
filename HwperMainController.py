d92239cda41a4376b7476df971634c276eb246c8
sookcha <lsookcha@me.com>
2014-04-23 16:00:30 +0900

    Finishing odt processing

--- HwperMainController.py
+++ HwperMainController.py
@@ -10,40 +10,65 @@ from PyObjCTools import AppHelper
 from Cocoa import *
 from Foundation import *
 
+import os
+import os.path
+import logging
+import sys
+import urllib
+import urllib2
+import tempfile
+import StringIO
+
+from hwp5.dataio import ParseError
+from hwp5.xmlmodel import Hwp5File
+from tempfile import mkstemp
+
 from hwp5 import __version__ as version
 from hwp5.proc import rest_to_docopt
 from hwp5.proc import init_logger
 from hwp5.errors import InvalidHwp5FileError
+from hwp5.hwp5odt import *
+from tempfile import mkstemp
+from hwp5.plat import get_xslt
 from hwp5.hwp5odt import hwp5_resources_filename
 from lxml import etree
 from docopt import docopt
+
+
 
 class HwperMainController(NSWindowController):
     searchFileButton = objc.IBOutlet()
     odtButton = objc.IBOutlet()
     label = objc.IBOutlet()
     label2 = objc.IBOutlet()
-    
+
     fileName = ""
-    
+
     def windowDidLoad(self):
         NSWindowController.windowDidLoad(self)
-        
+
     @objc.IBAction
     def toODT_(self, sender):
-        hwpFileName = self.label.getStringValue
-        self.label2.setStringValue_(hwpFileName)
- 
+    	
+         
+      xslt = plat.get_xslt()
+      rng = plat.get_relaxng()
+      
+      hwp5file = Hwp5File("/Users/sookcha/Desktop/Airkorea.hwp")
+      
+      ODTSingleDocumentConverter(xslt, rng).convert_to(hwp5file,"/Users/sookcha/Desktop/a.odt")
+
     @objc.IBAction
     def fileSearch_(self, sender):
         panel = NSOpenPanel.openPanel()
         panel.setCanCreateDirectories_(True)
         panel.setCanChooseDirectories_(True)
         panel.setCanChooseFiles_(True)
-        if panel.runModal() == NSOKButton:          
+        if panel.runModal() == NSOKButton:
           self.label.setStringValue_(panel.filename())
+          fileName = panel.filename()
         return
-        
+
     def hwp5odt(hwp5file, base_dir):
       """docstring for hwp5odt"""
 
@@ -52,6 +77,6 @@ if __name__ == "__main__":
     viewController = HwperMainController.alloc().initWithWindowNibName_("HwperMain")
     viewController.showWindow_(viewController)
     NSApp.activateIgnoringOtherApps_(True)
-    
+
     from PyObjCTools import AppHelper
     AppHelper.runEventLoop()
